b9d0bbfc3336fc57636c55c7ba23749da208cee0
mehdiicodes <akbarrr1026@gmail.com>
2024-08-11 12:34:25 +0530

    backend

---
+++ connection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const url = "mongodb+srv://mehdiicodes:akbarMehdi@akbarmehdi.q8tbfar.mongodb.net/?retryWrites=true&w=majority&appName=AkbarMehdi"
+
+//Connect to the Database
+
+//asynchronous - returns a promise
+mongoose.connect(url)
+.then((result) => {
+    console.log('connected to database');
+    
+})
+.catch((err) => {
+    console.log(err);
+    
+});
+
